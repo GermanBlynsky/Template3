c2246648c400cdaadb53d8120c1d6ba687bf2fce
GermanBlynsky <german.b05@mai.ru>
2024-11-03 23:41:14 +0300

    поменял кодировку

--- Template3/main.cpp
+++ Template3/main.cpp
@@ -3,32 +3,32 @@
 
 
 /**
-* @brief Точка входа в программу
-* @return 0 в случае успеха
+* @brief РўРѕС‡РєР° РІС…РѕРґР° РІ РїСЂРѕРіСЂР°РјРјСѓ
+* @return 0 РІ СЃР»СѓС‡Р°Рµ СѓСЃРїРµС…Р°
 */
 int main()
 {
 	std::setlocale(NULL, "ru_RU.UTF-8");
 
 	rut_miit::LinkedList<int> list{ 1, 2, 3, 4 };
-	std::cout << "Первоначальный список: " << list << std::endl;
+	std::cout << "РџРµСЂРІРѕРЅР°С‡Р°Р»СЊРЅС‹Р№ СЃРїРёСЃРѕРє: " << list << std::endl;
 
 	list.push_front(5);
-	std::cout << "Список после push_front(5): " << list << std::endl;
+	std::cout << "РЎРїРёСЃРѕРє РїРѕСЃР»Рµ push_front(5): " << list << std::endl;
 
 	list.push_back(5);
-	std::cout << "Список после push_back(5): " << list << std::endl;
+	std::cout << "РЎРїРёСЃРѕРє РїРѕСЃР»Рµ push_back(5): " << list << std::endl;
 
 	list.pop_front();
-	std::cout << "Список после pop_front(): " << list << std::endl;
+	std::cout << "РЎРїРёСЃРѕРє РїРѕСЃР»Рµ pop_front(): " << list << std::endl;
 
 	list.pop_back();
-	std::cout << "Список после pop_back(): " << list << std::endl;
+	std::cout << "РЎРїРёСЃРѕРє РїРѕСЃР»Рµ pop_back(): " << list << std::endl;
 
 	list.insert(3, 10);
-	std::cout << "Список после insert " << list << std::endl;
+	std::cout << "РЎРїРёСЃРѕРє РїРѕСЃР»Рµ insert " << list << std::endl;
 
 	list.remove(0);
-	std::cout << "Список после remove(3): " << list << std::endl;
+	std::cout << "РЎРїРёСЃРѕРє РїРѕСЃР»Рµ remove(3): " << list << std::endl;
 	return 0;
 }
